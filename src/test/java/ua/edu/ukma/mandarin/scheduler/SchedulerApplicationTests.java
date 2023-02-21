f3da1555c6a472250481a997586bd56966b37157
Anastasiia Bezruka <nastyabezruka@gmail.com>
2023-02-21 21:17:33 +0100

    Project template

---
+++ src/test/java/ua/edu/ukma/mandarin/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.mandarin.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
