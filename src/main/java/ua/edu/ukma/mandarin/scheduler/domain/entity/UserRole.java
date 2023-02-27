800da21f112d50aafcc9a4e593ff7280fa80175e
OleksandRomaniuk <oleksandr.romaniuk@teamdev.com>
2023-02-27 18:03:40 +0200

    Add different kind of users

---
+++ src/main/java/ua/edu/ukma/mandarin/scheduler/domain/entity/UserRole.java
@@ -0,0 +1,7 @@
+package ua.edu.ukma.mandarin.scheduler.domain.entity;
+
+public enum UserRole {
+    STUDENT,
+    TEACHER,
+    SUPERVISOR;
+}
---
+++ src/main/java/ua/edu/ukma/mandarin/scheduler/repository/TeacherRepository.java
@@ -0,0 +1,12 @@
+package ua.edu.ukma.mandarin.scheduler.repository;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ua.edu.ukma.mandarin.scheduler.domain.entity.Teacher;
+
+@Repository
+public interface TeacherRepository extends CrudRepository<Teacher, Integer> {
+    Teacher getByUserId(Integer id);
+}
