9223a53868b9ef76a453b44b00b4b6b8ce423035
Anastasiia Bezruka <nastyabezruka@gmail.com>
2023-03-06 18:24:31 +0200

    Add SQL scripts

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,3 +16,30 @@ CREATE TABLE principal_role (
     CONSTRAINT principal_id_fk FOREIGN KEY (principal_id) REFERENCES principal(id),
     CONSTRAINT role_id_fk FOREIGN KEY (role_id) REFERENCES role(id)
 );
+
+CREATE TABLE subject (
+    id bigint PRIMARY KEY,
+    name varchar(255) NOT NULL UNIQUE,
+    author bigserial NOT NULL,
+    CONSTRAINT author_fk FOREIGN KEY (author) REFERENCES -- TODO: Sasha needs to add
+);
+
+CREATE TABLE group (
+    id bigint PRIMARY KEY,
+    number smallint NOT NULL
+                   check (number > 1 and number < 10),
+    lecturer bigserial NOT NULL,
+    subject bigint NOT NULL,
+    CONSTRAINT subject_fk FOREIGN KEY (subject) REFERENCES subject(id)
+    CONSTRAINT lecturer_fk FOREIGN KEY (lecturer) REFERENCES --TODO: Sasha needs to add
+);
+
+CREATE TABLE group_students (
+    group_id bigint NOT NULL,
+    student_id bigserial NOT NULL,
+    CONSTRAINT group_student_pk PRIMARY KEY (group_id, student_id),
+    CONSTRAINT group_id_fk FOREIGN KEY (group_id) REFERENCES group(id),
+    CONSTRAINT student_id_fk FOREIGN KEY (student_id) REFERENCES --TODO: Sasha needs to add
+);
+
+--TODO: users & schedules tables
